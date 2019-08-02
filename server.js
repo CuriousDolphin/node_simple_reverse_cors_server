754bba71699e72a5a7a29d2a5a17142ac5f7ec10
IvanMurabito <i.murabito@enerbrain.com>
2019-08-02 14:49:01 +0200

    non https

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ cors_proxy.createServer({
     originWhitelist: [], // Allow all origins
     requireHeader: ['origin', 'x-requested-with'],
     removeHeaders: ['cookie', 'cookie2'],
-    httpsOptions: true
+    //httpsOptions: true
 }).listen(port, host, function() {
     console.log('Running CORS Anywhere on ' + host + ':' + port);
 });
